3cb3105dc06f661828f8630e4ea4be9cd7c7b852
Falcol <bvngoc2000@gmail.com>
2022-03-04 08:39:56 +0700

    test command . rails g controller hello index .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello', to:'hello#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
