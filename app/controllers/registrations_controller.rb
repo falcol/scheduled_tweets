a569a11161a87ee3174d974ef5fe8983f78556d9
Falcol <bvngoc2000@gmail.com>
2022-03-07 13:45:31 +0700

    front better

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,8 +6,7 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id
-            redirect_to root_path, notice: 'User was successfully created.'
+            redirect_to sign_in_path, notice: 'User was successfully created.'
         else
             flash[:alert] = 'User was not created.'
             render :new
